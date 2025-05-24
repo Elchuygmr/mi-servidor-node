921efe992cca4d11e55f83a9290dbc632e0a481c
Elchuygmr <gabo54024@gmail.com>
2025-05-24 11:55:42 -0600

    Pedidos.js

--- server/public/js/Pedidos.js
+++ server/public/js/Pedidos.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const BASE_URL = 'http://localhost:3000';
+    const BASE_URL = 'https://mi-servidor-node-8fas.onrender.com';
     const pedidoForm = document.getElementById("pedidoForm");
     const productosContainer = document.getElementById("productosContainer");
     const listaPedido = document.getElementById("listaPedido");
